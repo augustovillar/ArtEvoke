6bf8ff3c7606d82bd7a070b4b943145f5c1aaa99
Augusto Villar <augusto.villar@usp.br>
2025-05-28 18:54:46 +0200

    fix: final version of the files

--- project_assets/tests/LMMs/llama/testLlama.py
+++ project_assets/tests/LMMs/llama/testLlama.py
@@ -36,17 +36,8 @@ model = MllamaForConditionalGeneration.from_pretrained(
 processor = AutoProcessor.from_pretrained(model_id)
 
 
-def load_image(image_file):
-    # if image_file.startswith("http") or image_file.startswith("https"):
-    #     response = requests.get(image_file)
-    #     image = Image.open(BytesIO(response.content)).convert("RGB")
-    # else:
-    image = Image.open(image_file).convert("RGB")
-    return image
-
-
 def process_image(image_path):
-    image = load_image(image_path)
+    image = Image.open(image_path).convert("RGB")
     messages = [
         {
             "role": "user",
